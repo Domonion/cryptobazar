dcb79f783e6b87270836cd8e79e625ec0d6c55a7
Georgiy Nazarov <geranazavr555@yandex.ru>
2018-09-25 19:20:52 +0300

    a skeleton for app

---
+++ app/constructor.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function gen() {
+    var form = document.forms[0];
+
+    var name_ico = form.elements.name.value;
+    var abb_ico = form.elements.abb.value;
+    var token_count = form.elements.token_count.value;
+    var start_date = form.elements.start_d.value;
+    var time_ico = form.elements.time_ico.value;
+
+
+}
